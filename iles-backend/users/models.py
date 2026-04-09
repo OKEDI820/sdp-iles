5bd7e87812fec634948521534cd2b0176e3a90c6
Okedi Ismail Musa <okediismail820@gmail.com>
2026-04-09 23:51:20 +0300

    created custom user model

--- iles-backend/users/models.py
+++ iles-backend/users/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    ROLE_CHOICES = (
+        ('admin', 'Admin'),
+        ('student', 'Student'),
+        ('supervisor', 'Supervisor'),
+    )
+
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='student')
+
+    def __str__(self):
+        return self.username
