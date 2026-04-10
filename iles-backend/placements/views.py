a72d5c677f039016f93e5eced160adac2260f269
Okedi Ismail Musa <okediismail820@gmail.com>
2026-04-10 11:57:54 +0300

    adjusted views for placement

--- iles-backend/placements/views.py
+++ iles-backend/placements/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Placement
+from .serializers import PlacementSerializer
 
-# Create your views here.
+
+class PlacementViewSet(viewsets.ModelViewSet):
+    queryset = Placement.objects.all().order_by('-created_at')
+    serializer_class = PlacementSerializer
+    permission_classes = [IsAuthenticated]
