d9a10a52098715e630415bf9bdbec1054fe78d3e
Okedi Ismail Musa <okediismail820@gmail.com>
2026-04-10 12:38:15 +0300

    role protection for placements

--- iles-backend/placements/views.py
+++ iles-backend/placements/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
 from .models import Placement
 from .serializers import PlacementSerializer
-
+from users.permissions import IsAdminUserRole
 
 class PlacementViewSet(viewsets.ModelViewSet):
     queryset = Placement.objects.all().order_by('-created_at')
