67802b8fc6b187ff981bd7f4218cea67673d2005
Okedi Ismail Musa <okediismail820@gmail.com>
2026-04-11 23:22:40 +0300

    made log serializers and viewset

--- iles-backend/logs/views.py
+++ iles-backend/logs/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import WeeklyLog
+from .serializers import WeeklyLogSerializer
 
-# Create your views here.
+class WeeklyLogViewSet(viewsets.ModelViewSet):
+    queryset = WeeklyLog.objects.all().order_by("-created_at")
+    serializer_class = WeeklyLogSerializer
+    permission_classes = [IsAuthenticated]
