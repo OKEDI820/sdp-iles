767ebb79b1e2eb449d6605827d7fcdb96d2475c8
mmark6 <mmukisa64@gmail.com>
2026-04-13 07:39:05 +0300

    Fix user name key in response dictionary
    Corrected the key for user name in the response.

--- iles-backend/users/views.py
+++ iles-backend/users/views.py
@@ -13,7 +13,7 @@ class CurrentUserView(APIView):
             "username": request.user.username,
             "email": request.user.email,
             "role": request.user.role,
-            "name": request.user.role,
+            
             
         })
 # Create your views here.
