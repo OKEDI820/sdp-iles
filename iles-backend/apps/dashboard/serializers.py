af00603869140fa892d7c950e628a70fab5172e0
OKEDI820 <okediismail820@gmail.com>
2026-04-23 23:35:55 +0300

    Fix formatting of DashboardSummarySerializer fields

--- iles-backend/apps/dashboard/serializers.py
+++ iles-backend/apps/dashboard/serializers.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 
 class DashboardSummarySerializer(serializers.Serializer):
     total_users = serializers.IntegerField(),
-    total_placements = serializers.IntegerField() 
+    total_placements = serializers.IntegerField(), 
     total_logs = serializers.IntegerField()
     draft_logs = serializers.IntegerField()
     submitted_logs = serializers.IntegerField()
