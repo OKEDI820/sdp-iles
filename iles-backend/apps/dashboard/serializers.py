d748ba63d008f9241f063a9427e25890c6d1b8ee
OKEDI820 <okediismail820@gmail.com>
2026-04-23 23:37:58 +0300

    Fix syntax for integer fields in serializer

--- iles-backend/apps/dashboard/serializers.py
+++ iles-backend/apps/dashboard/serializers.py
@@ -2,10 +2,10 @@ from rest_framework import serializers
 
 
 class DashboardSummarySerializer(serializers.Serializer):
-    total_users = serializers.IntegerField(),
-    total_placements = serializers.IntegerField(), 
-    total_logs = serializers.IntegerField(),
-    draft_logs = serializers.IntegerField(),
+    total_users = serializers.IntegerField()
+    total_placements = serializers.IntegerField()
+    total_logs = serializers.IntegerField()
+    draft_logs = serializers.IntegerField()
     submitted_logs = serializers.IntegerField()
     reviewed_logs = serializers.IntegerField()
     approved_logs = serializers.IntegerField()
