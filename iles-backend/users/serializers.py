c0b5b812cf88f25fdba22d2d1da9a6869c3dfdb3
Okedi Ismail Musa <okediismail820@gmail.com>
2026-04-23 23:14:14 +0300

    improved user serializer

--- iles-backend/users/serializers.py
+++ iles-backend/users/serializers.py
@@ -1,8 +1,7 @@
 from rest_framework import serializers
 from .models import User
 
-
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'role']
+        fields = ['id', 'username', 'email', 'role', 'first_name', 'last_name']
