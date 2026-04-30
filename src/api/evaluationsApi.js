c0643b8283c11a8a4499af6d79127eeea773f253
mmark6 <mmukisa64@gmail.com>
2026-05-01 00:51:41 +0300

    Fix syntax error in createEvaluation function

--- src/api/evaluationsApi.js
+++ src/api/evaluationsApi.js
@@ -1,4 +1,4 @@
 import api from './axios'
 export const getEvaluations = () => api.get('/evaluations/')
-export const createEvaluation = (payload) => api.post('/evaluations/', payload)
+export const createEvaluation = => api.post('/evaluations/', payload)
 export const updateEvaluation = (id, payload) => api.patch(`/evaluations/${id}/`, payload)
