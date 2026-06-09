fe8c1df4e9c69c2741b03362c9f029b8e5dffb82
mmark6 <mmukisa64@gmail.com>
2026-06-09 15:10:51 +0300

    Switch database configuration to use dj-database-url

--- iles-backend/config/settings.py
+++ iles-backend/config/settings.py
@@ -72,12 +72,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
-
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
